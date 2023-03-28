ff73155c11f281a9feeca65e274a4e365506e302
Vladimir RM <shvdrummer@gmail.com>
2023-03-28 13:15:59 +0300

    Destructuring done.

---
+++ destructuriation.js
@@ -0,0 +1,104 @@
+// const auto = {
+//   name: "Tesla",
+//   year: 2020,
+//   color: "red",
+//   dimensions: {
+//     size: {
+//       width: "1.99mm",
+//       length: "1,55mm",
+//     },
+//   },
+// };
+
+// const { width, length } = auto.dimensions.size;
+// console.log(width, length);
+
+// ///////////Get value default/////////////////////
+
+// const auto = {
+//   name: "Tesla",
+// };
+
+// const { name, year = 2020 } = auto;
+// console.log(name, year);
+
+// //////////////////Get value properties//////////
+
+// const auto = ["Tesla", 2020,'Green']
+
+// const [name, year, color] = auto
+// console.log(name, year, color);
+
+//////////////  /////////////////
+
+// const auto = 'Tesla, 2020,Green'
+
+// const [name, year, color] = auto.split(',')
+// console.log(name, year, color);
+
+////////////  /////////////////
+
+// function convertRubles(rubles) {
+//   const complete = {
+//     EUR: rubles * 80,
+//     USD: rubles * 70,
+//     GBP: rubles * 100,
+//   };
+//   return complete;
+// }
+
+// const { EUR, USD, GBP } = convertRubles(5000);
+// console.log(EUR, USD, GBP );
+
+// const auto = {
+//   name: "Tesla",
+//   year: 2020,
+//   color: "red",
+//   dimensions: {
+//     size: {
+//       width: "1.99mm",
+//       length: "1,55mm",
+//     },
+//   },
+// };
+
+// const { width, length } = auto.dimensions.size;
+// console.log(width, length);
+
+// ///////////Get value default/////////////////////
+
+// const auto = {
+//   name: "Tesla",
+// };
+
+// const { name, year = 2020 } = auto;
+// console.log(name, year);
+
+// //////////////////Get value properties//////////
+
+// const auto = ["Tesla", 2020,'Green']
+
+// const [name, year, color] = auto
+// console.log(name, year, color);
+
+//////////////  /////////////////
+
+// const auto = 'Tesla, 2020,Green'
+
+// const [name, year, color] = auto.split(',')
+// console.log(name, year, color);
+
+////////////  /////////////////
+
+// function convertRubles(rubles) {
+//   const complete = {
+//     EUR: rubles * 80,
+//     USD: rubles * 70,
+//     GBP: rubles * 100,
+//   };
+//   return complete;
+// }
+
+// const { EUR, USD, GBP } = convertRubles(5000);
+// console.log(EUR, USD, GBP );
+
--- script.js
+++ script.js
@@ -1,51 +1,7 @@
-// const auto = {
-//   name: "Tesla",
-//   year: 2020,
-//   color: "red",
-//   dimensions: {
-//     size: {
-//       width: "1.99mm",
-//       length: "1,55mm",
-//     },
-//   },
-// };
+// function getTotal ({total,shipment = 0.1}){
+//   return total + total * shipment
+// }
 
-// const { width, length } = auto.dimensions.size;
-// console.log(width, length);
+// const total = getTotal({shipment: 0.2, total: 1000})
 
-// ///////////Get value default/////////////////////
-
-// const auto = {
-//   name: "Tesla",
-// };
-
-// const { name, year = 2020 } = auto;
-// console.log(name, year);
-
-// //////////////////Get value properties//////////
-
-// const auto = ["Tesla", 2020,'Green']
-
-// const [name, year, color] = auto
-// console.log(name, year, color);
-
-//////////////  /////////////////
-
-// const auto = 'Tesla, 2020,Green'
-
-// const [name, year, color] = auto.split(',')
-// console.log(name, year, color);
-
-////////////  /////////////////
-
-function convertRubles(rubles) {
-  const complete = {
-    EUR: rubles * 80,
-    USD: rubles * 70,
-    GBP: rubles * 100,
-  };
-  return complete;
-}
-
-const { EUR, USD, GBP } = convertRubles(5000);
-console.log(EUR, USD, GBP );
+// console.log(total);
