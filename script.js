e0db4d7596a271fa221889bf3ef69010c99dd61b
Vladimir RM <shvdrummer@gmail.com>
2023-03-28 10:08:01 +0300

    Add car arr.

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 ////////////rest////////////
 
-function convertUsd(rate, ...totals) {
-  console.log(rate, totals);
-  return totals.map((total) => total / rate);
-}
+// function convertUsd(rate, ...totals) {
+//   console.log(rate, totals);
+//   return totals.map((total) => total / rate);
+// }
 
- const total = convertUsd(70, 1000, 5000, 800);
+//  const total = convertUsd(70, 1000, 5000, 800);
 
-console.log(total);
+// console.log(total);
+const car = ["Tesla", 111, "red", 50000, "10%", "4Wd"];
