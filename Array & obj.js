c27059cb6c2dd564f4a44f37de762a1088a3d357
Vladimir RM <shvdrummer@gmail.com>
2023-03-28 14:34:22 +0300

    Array task done.

---
+++ Array & obj.js
@@ -0,0 +1,54 @@
+// let name = "Vasiliy";
+// let twoName = name;
+// name = "Stas";
+
+// console.log(name);
+// console.log(twoName);
+
+// let age = 10;
+// let ageTwo = 15;
+// age = 25;
+
+// console.log(age);
+// console.log(ageTwo);
+
+////////////////////insert in array/////////
+
+// let students = ["Ivan", "Petro ", "vasiliy"];
+
+// const group = students;
+
+// group[2] = "Yanina";
+
+// console.log(students);
+// console.log(group);
+///////////// copy use any method///////////////
+
+
+/////////////slice copy el in arr////////////
+// let students = ["Ivan", "Petro ", "vasiliy"];
+
+// const group2 = students.slice();
+// group2[2] = "ivan";
+// console.log(students);
+// console.log(group2);
+
+// ////////////////////copy use concat //////////////////////
+
+// let students = ["Ivan", "Petro ", "vasiliy"];
+
+// const group2 = [].concat(students);
+
+//////////////// copy using spread ///////////////
+
+// let students = ["Ivan", "Petro ", "vasiliy"];
+
+// const group = [...students];
+// console.log(group);
+
+//////////copy using Array from ////////
+
+// let students = ["Ivan", "Petro ", "vasiliy"];
+
+// const group = Array.from(students);
+// console.log(group);
