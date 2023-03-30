612c327544c78a3e5fd4ce203d2352ec981f5402
Vladimir RM <shvdrummer@gmail.com>
2023-03-30 20:00:31 +0300

    Add second method.

--- Template strings.js
+++ Template strings.js
@@ -1,8 +1,39 @@
-const car = {
-    name: 'Tesla',
+/////////////first method /////////
+
+// const car = {
+//   name: "Tesla",
+//   year: 2010,
+// };
+
+// const html = `
+
+// <div>
+//    <h1>${car.name}</h1>
+//   <p>${car.year}</p>
+
+// </div>
+
+// `;
+// document.body.innerHTML = html;
+
+////////////////////second render ///////////
+
+const cars = [
+  {
+    name: "Tesla",
     year: 2010,
-}
+  },
+  {
+    name: "Nissan",
+    year: 2012,
+  },
+];
 
 const html = `
+<ul>
+${cars.map((car) => `${car.name} ${car.year}`).join(" ")}
+</ul>
+`;
 
-`
+document.body.innerHTML = html;
+console.log(html);
