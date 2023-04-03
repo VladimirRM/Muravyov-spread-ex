0824c5bf735f530cc43cd70237c2cf0f33b22488
Vladimir RM <shvdrummer@gmail.com>
2023-04-03 18:14:34 +0300

    Add adress api.

--- promise.js
+++ promise.js
@@ -1,3 +1,5 @@
-console.log('Start');
-console.log();
-console.log();
+console.log("Start");
+
+const listPromise = fetch('https://api.sampleapis.com/countries/countries')
+console.log(" Get done");
+console.log(`Countries >`, list );
