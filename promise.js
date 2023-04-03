2c19861674268d1a6e77da80c008b2f85cd3579a
Vladimir RM <shvdrummer@gmail.com>
2023-04-03 18:18:38 +0300

    Add then and get data in log.

--- promise.js
+++ promise.js
@@ -1,5 +1,4 @@
-console.log("Start");
-
-const listPromise = fetch('https://api.sampleapis.com/countries/countries')
-console.log(" Get done");
-console.log(`Countries >`, list );
+const listPromise = fetch("https://api.sampleapis.com/countries/countries");
+listPromise.then((data) => {
+  console.log(data);
+});
