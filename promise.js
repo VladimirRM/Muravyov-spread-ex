3777553d9537a0ac727d2400aedb56a2461f6876
Vladimir RM <shvdrummer@gmail.com>
2023-04-04 17:50:06 +0300

    Comment var three.

--- promise.js
+++ promise.js
@@ -20,36 +20,45 @@
 //     console.log("Err");
 //   });
 
-const family = [
-  { member: "Mom", id: 111, coffee: "Latte" },
-  { member: "Father", id: 222, coffee: "Espresso" },
-  { member: "Sun", id: 333, coffee: "Cappucino" },
-];
+// const family = [
+//   { member: "Mom", id: 111, coffee: "Latte" },
+//   { member: "Father", id: 222, coffee: "Espresso" },
+//   { member: "Sun", id: 333, coffee: "Cappucino" },
+// ];
 
-const getCoffee = (member) => {
-  const coffeePromise = fetch("https://api.sampleapis.com/coffee/hot");
-  return coffeePromise
-    .then((data) => data.json())
-    .then((list) => {
-      console.log("list >>>", list);
-    });
-};
+// const getCoffee = (member) => {
+//   const coffeePromise = fetch("https://api.sampleapis.com/coffee/hot");
+//   return coffeePromise
+//     .then((data) => data.json())
+//     .then((list) => {
+//       const coffee = list.find((res) => res.title === member.coffee);
+//       console.log(coffee);
+//       return {
+//         ...member,
+//         coffee,
+//       };
+//     });
+// };
 
-const getFamilyMember = (id) => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      const member = family.find((res) => res.id === id);
-      console.log(member);
-      if (member) {
-        resolve(member);
-      } else {
-        reject(Error("member not found"));
-      }
-    }, 1500);
-  });
-};
+// const getFamilyMember = (id) => {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       const member = family.find((res) => res.id === id);
+//       console.log(member);
+//       if (member) {
+//         resolve(member);
+//       } else {
+//         reject(Error("member not found"));
+//       }
+//     }, 1500);
+//   });
+// };
 
-getFamilyMember(111).then((data) => {
-  console.log(data);
-  getCoffee(data);
-});
+// getFamilyMember(000)
+//   .then((data) => getCoffee(data))
+//   .then((newMember) => {
+//     console.log("newMember", newMember);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
